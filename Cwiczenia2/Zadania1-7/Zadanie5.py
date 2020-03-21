0aca42a07a6190e066a821436cfc63c86065f231
lukasz49 <61496489+lukasz49@users.noreply.github.com>
2020-03-21 16:00:54 +0100

    Zadania 1-7

---
+++ Cwiczenia2/Zadania1-7/Zadanie5.py
@@ -0,0 +1,4 @@
+imie = "Lukasz"
+nazwisko = "Ziolkowski"
+
+print(imie[::-1].capitalize() + " " + nazwisko[::-1].capitalize())
---
+++ Cwiczenia2/Zadania1-7/Zadanie6.py
@@ -0,0 +1,7 @@
+lista_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+lista_2 = list(lista_1[5:])
+lista_1 = list(lista_1[:5])
+
+print(lista_1)
+print(lista_2)
