9c5616557446f7dced9df28a5d5e5a9002967ad4
patrykk96 <136073@student.uwm.edu.pl>
2020-05-06 08:39:37 +0200

    crud operations for movies in redux

--- Projekt/frontend/src/store/actions/movieActions.js
+++ Projekt/frontend/src/store/actions/movieActions.js
@@ -34,3 +34,144 @@ export const movieAdd = movie => {
              });
     }
 }
+
+export const movieEditStart = () => {
+    return {
+        type: types.MOVIE_EDIT_START
+    }
+}
+
+export const movieEditSuccess = response => {
+    return {
+        type: types.MOVIE_EDIT_SUCCESS,
+        response: response
+    }
+}
+
+export const movieEditFailed = error => {
+    return {
+        type: types.MOVIE_EDIT_FAILED,
+        error: error
+    }
+}
+
+export const movieEdit = movie => {
+    return dispatch => {
+        dispatch(movieEditStart());
+
+        const formData = new FormData();
+        formData.append("Image", movie.image);
+
+        axios.patch("url do uzupelnienia")
+             .then(response => {
+                 dispatch(movieEditSuccess(response.data));
+             })
+             .catch(error => {
+                 dispatch(movieEditFailed(error.response.status));
+             });
+    }
+}
+
+export const movieGetStart = () => {
+    return {
+        type: types.MOVIE_GET_START
+    }
+}
+
+export const movieGetSuccess = response => {
+    return {
+        type: types.MOVIE_GET_SUCCESS,
+        response: response
+    }
+}
+
+export const movieGetFailed = error => {
+    return {
+        type: types.MOVIE_GET_FAILED,
+        error: error
+    }
+}
+
+export const movieGet = movie => {
+    return dispatch => {
+        dispatch(movieGetStart());
+
+        axios.get("url do uzupelnienia")
+             .then(response => {
+                 dispatch(movieGetSuccess(response.data));
+             })
+             .catch(error => {
+                 dispatch(movieGetFailed(error.response.status));
+             });
+    }
+}
+
+export const moviesGetStart = () => {
+    return {
+        type: types.MOVIES_GET_START
+    }
+}
+
+export const moviesGetSuccess = response => {
+    return {
+        type: types.MOVIES_GET_SUCCESS,
+        response: response
+    }
+}
+
+export const moviesGetFailed = error => {
+    return {
+        type: types.MOVIES_GET_FAILED,
+        error: error
+    }
+}
+
+export const moviesGet = () => {
+    return dispatch => {
+        dispatch(moviesGetStart());
+
+        axios.get("url do uzupelnienia")
+             .then(response => {
+                 dispatch(moviesGetSuccess(response.data));
+             })
+             .catch(error => {
+                 dispatch(moviesGetFailed(error.response.status));
+             });
+    }
+}
+
+export const movieDeleteStart = () => {
+    return {
+        type: types.MOVIE_DELETE_START
+    }
+}
+
+export const movieDeleteSuccess = response => {
+    return {
+        type: types.MOVIE_DELETE_SUCCESS,
+        response: response
+    }
+}
+
+export const movieDeleteFailed = error => {
+    return {
+        type: types.MOVIE_DELETE_FAILED,
+        error: error
+    }
+}
+
+export const movieDelete = movie => {
+    return dispatch => {
+        dispatch(movieDeleteStart());
+
+        axios.patch("url do uzupelnienia")
+             .then(response => {
+                 dispatch(movieDeleteSuccess(response.data));
+             })
+             .catch(error => {
+                 dispatch(movieDeleteFailed(error.response.status));
+             });
+    }
+}
+
+
--- Projekt/frontend/src/store/reducers/movieReducer.js
+++ Projekt/frontend/src/store/reducers/movieReducer.js
@@ -4,7 +4,7 @@ import { updateObject } from '../utility';
 const initialState = {
     error: null,
     response: null,
-    loading: false
+    loading: false,
 }
 
 const movieAddStart = state => {
@@ -26,11 +26,105 @@ const movieAddFailed = (state, action) => {
     });
 }
 
+const movieEditStart = state => {
+    return updateObject(state, {loading: true});
+}
+
+const movieEditSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: false,
+        response: action.response
+    });
+}
+
+const movieEditFailed = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+}
+
+const movieGetStart = state => {
+    return updateObject(state, {loading: true});
+}
+
+const movieGetSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: false,
+        response: action.response
+    });
+}
+
+const movieGetFailed = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+}
+
+const moviesGetStart = state => {
+    return updateObject(state, {loading: true});
+}
+
+const moviesGetSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: false,
+        response: action.response
+    });
+}
+
+const moviesGetFailed = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+}
+
+const movieDeleteStart = state => {
+    return updateObject(state, {loading: true});
+}
+
+const movieDeleteSuccess = (state, action) => {
+    return updateObject(state, {
+        error: null,
+        loading: false,
+        response: action.response
+    });
+}
+
+const movieDeleteFailed = (state, action) => {
+    return updateObject(state, {
+        error: action.error,
+        loading: false
+    });
+}
+
+
+
 const movieReducer = (state = initialState, action) => {
     switch(action.type){
         case actionTypes.MOVIE_ADD_START: return movieAddStart(state);
         case actionTypes.MOVIE_ADD_SUCCESS: return movieAddSuccess(state, action);
         case actionTypes.MOVIE_ADD_FAILED: return movieAddFailed(state, action);
+
+        case actionTypes.MOVIE_EDIT_START: return movieEditStart(state);
+        case actionTypes.MOVIE_EDIT_SUCCESS: return movieEditSuccess(state, action);
+        case actionTypes.MOVIE_EDIT_FAILED: return movieEditFailed(state, action);
+
+        case actionTypes.MOVIE_GET_START: return movieGetStart(state);
+        case actionTypes.MOVIE_GET_SUCCESS: return movieGetSuccess(state, action);
+        case actionTypes.MOVIE_GET_FAILED: return movieGetFailed(state, action);
+
+        case actionTypes.MOVIES_GET_START: return moviesGetStart(state);
+        case actionTypes.MOVIES_GET_SUCCESS: return moviesGetSuccess(state, action);
+        case actionTypes.MOVIES_GET_FAILED: return moviesGetFailed(state, action);
+
+        case actionTypes.MOVIE_DELETE_START: return movieDeleteStart(state);
+        case actionTypes.MOVIE_DELETE_SUCCESS: return movieDeleteSuccess(state, action);
+        case actionTypes.MOVIE_DELETE_FAILED: return movieDeleteFailed(state, action);
 
         default: return state
     };
